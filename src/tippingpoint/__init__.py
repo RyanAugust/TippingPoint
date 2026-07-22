4b4203a5048e7276240500ab4f2678601c7e0fe9
RyanAugust <ryan.duecker@yahoo.com>
2026-07-22 17:44:20 +0000

    bump version

--- src/tippingpoint/__init__.py
+++ src/tippingpoint/__init__.py
@@ -1,6 +1,6 @@
 __name__ = "tippingpoint"
 __author__ = "Ryan Duecker"
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 from .models import MarketingReturnCurve as MarketingReturnCurve
 from .portfolio import PortfolioAllocator as PortfolioAllocator
