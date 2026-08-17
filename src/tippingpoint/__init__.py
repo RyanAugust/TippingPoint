3b2ee094991994d26d8687ee8334a92cae8a06b3
Ryan Duecker <ryan.duecker@gmail.com>
2026-08-17 16:35:35 -0400

    bump to 0.5.1

--- src/tippingpoint/__init__.py
+++ src/tippingpoint/__init__.py
@@ -1,6 +1,6 @@
 __name__ = "tippingpoint"
 __author__ = "Ryan Duecker"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 from .models import MarketingReturnCurve as MarketingReturnCurve
 from .portfolio import PortfolioAllocator as PortfolioAllocator
