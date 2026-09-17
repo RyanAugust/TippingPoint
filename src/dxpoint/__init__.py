289516802598df46a665a27c00aa5942ed91882f
RyanAugust <ryan.duecker@yahoo.com>
2026-09-17 17:05:27 +0000

    no import

--- src/dxpoint/__init__.py
+++ src/dxpoint/__init__.py
@@ -5,7 +5,6 @@ __version__ = "0.5.2"
 from .models import MarketingReturnCurve as MarketingReturnCurve
 from .portfolio import PortfolioAllocator as PortfolioAllocator
 from .multichannel import MultiChannelModel as MultiChannelModel
-from .multichannel import MultiChannelMMM as MultiChannelMMM
 from .math import weibull_adstock as weibull_adstock
 from .validation import validate_curve_experiments as validate_curve_experiments
 from .validation import validate_multichannel_experiments as validate_multichannel_experiments
